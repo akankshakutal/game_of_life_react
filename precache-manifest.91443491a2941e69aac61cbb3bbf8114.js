aed406096926cc408345859a10fab631519340b3
akankshakutal <44019583+akankshakutal@users.noreply.github.com>
2019-04-02 11:28:14 +0530

    Updates

---
+++ precache-manifest.91443491a2941e69aac61cbb3bbf8114.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cec782fae3dcdcf71e7c",
+    "url": "/game_of_life_react/static/js/runtime~main.1c3aaf3e.js"
+  },
+  {
+    "revision": "3bed8d47afa9132b29cc",
+    "url": "/game_of_life_react/static/js/main.b9579521.chunk.js"
+  },
+  {
+    "revision": "d342ab7bc6c0cbfd677d",
+    "url": "/game_of_life_react/static/js/2.693cc7fd.chunk.js"
+  },
+  {
+    "revision": "3bed8d47afa9132b29cc",
+    "url": "/game_of_life_react/static/css/main.b513ef7e.chunk.css"
+  },
+  {
+    "revision": "cb419e2cb1197ee3b75cf0bdd7d9e05e",
+    "url": "/game_of_life_react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/akankshakutal/game_of_life_react/precache-manifest.f50c9eba5362fde1c630572ec8867eb0.js"
+  "/game_of_life_react/precache-manifest.91443491a2941e69aac61cbb3bbf8114.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/akankshakutal/game_of_life_react/index.html", {
+workbox.routing.registerNavigationRoute("/game_of_life_react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
